a31e8ad14d85694b52b8c30c7a154b4290b9eb5a
edgar <edgar@host.docker.internal>
2025-07-20 13:27:31 -0600

    Imp Autenticador JWT paso 1/7

---
+++ src/main/java/mx/com/qtx/cotizadorv1ds/seguridad/jwt/AlgoritmoCifradoParLlaves.java
@@ -0,0 +1,14 @@
+package mx.com.qtx.cotizadorv1ds.seguridad.jwt;
+
+public enum AlgoritmoCifradoParLlaves {
+	EdDSA,
+	ES256,
+	ES384,
+	ES512,
+	PS256,
+	PS384,
+	PS512,
+	RS256,
+	RS384,
+	RS512
+}
